53955c3444100a2e2070c4d1bc81b619729fcced
NOAH LUBKO <G_NLOVKP@icc.co.il>
2020-08-06 12:23:34 +0300

    add webpack files

---
+++ webpack.dev.js
@@ -0,0 +1,6 @@
+const common = require("./webpack.common");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+};
---
+++ webpack.prod.js
@@ -0,0 +1,6 @@
+const common = require("./webpack.common");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/index.js",
+};
